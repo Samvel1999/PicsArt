d166a3b854f837eda4af0b64a33103fb8e269395
Samvel1999 <samvelkhachatryan1999@gmail.com>
2020-10-28 16:27:41 +0400

    Primitive Types

---
+++ Primitive Types/src/PrimitiveTypes.java
@@ -0,0 +1,29 @@
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+
+        //Primitive Types
+        byte b = 123;
+        short s = 1345;
+        int i = 113455;
+        long l = 143535435366536L;
+        float f = 12.6f;
+        double d = 19.3d;
+        char c = 'a';
+        char c1 = '\u028A';
+        boolean bool = true;
+
+        //Non-Primitive Type
+        String str = "This is String";
+
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(c1);
+        System.out.println(bool);
+        System.out.println(str);
+    }
+}
