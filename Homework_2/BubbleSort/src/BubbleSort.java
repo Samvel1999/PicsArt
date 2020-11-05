e65e72488b083aacb997cec3ff4611b7ba376f7d
Samvel1999 <samvelkhachatryan1999@gmail.com>
2020-11-05 04:02:58 +0400

    Changes in Bubble Sort

--- Homework_2/BubbleSort/src/BubbleSort.java
+++ Homework_2/BubbleSort/src/BubbleSort.java
@@ -1,19 +1,21 @@
 public class BubbleSort {
     public void sort(int[] arr) {
         int n = arr.length;
-        boolean sorted;
+        boolean isSorted;
 
         for(int i = 0; i < n - 1; i++) {
-            sorted = true;
+            isSorted = true;
             for(int j = 0; j < n - i - 1; j++) {
                 if(arr[j] > arr[j + 1]) {
+
                     arr[j] = arr[j] ^ arr[j + 1];
                     arr[j + 1] = arr[j] ^ arr[j + 1];
                     arr[j] = arr[j] ^ arr[j + 1];
-                    sorted = false;
+                    
+                    isSorted = false;
                 }
             }
-            if(sorted) {
+            if(isSorted) {
                 break;
             }
         }
